150b9be72e7d73dc1a9932376e409385282e947d
Eduardo <echavez@utea.edu.pe>
2024-11-20 11:07:14 -0500

    Array02

--- Arreglos/Array02.java
+++ Arreglos/Array02.java
@@ -1,5 +1,25 @@
 package Arreglos;
 
 public class Array02 {
-    
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        numeros[0] = 8;
+        numeros[1] = 33;
+        numeros[2] = 200;
+        numeros[3] = -100;
+        numeros[4] = 150;
+        numeros[5] = 11;
+        numeros[6] = 81;
+        numeros[7] = 18;
+        numeros[8] = 45;
+        numeros[9] = -8;
+
+        System.out.println("El array tiene 10 elementos ¿cuál de ellos quiere ver?");
+        System.out.println("ingrese un valor del 0 al 9");
+        
+        int indice = Integer.parseInt(System.console().readLine());
+        System.out.println("El elemento que se encuentra en la posición "+indice);
+        System.out.println("es el "+numeros[indice]);
+
+    }
 }
