467b9de6ff9942a5b8ad471fa1cfab807636faf6
Eduardo <echavez@utea.edu.pe>
2024-11-27 10:52:47 -0500

    Ejercicio 3 completo

--- Arreglos/Ejercicio03.java
+++ Arreglos/Ejercicio03.java
@@ -16,8 +16,28 @@ public class Ejercicio03 {
         //rellenar el array con 100 numeros aleatorios
         for ( i = 0; i < numeros.length; i++) {
             numeros[i] = (int)(Math.random()*21);
-            System.out.print(numeros[i] + " ");
+            System.out.print(numeros[i] + "\t ");
+            if ((i+1)%10==0) {
+                System.out.println();
+            }
         }
+
+        System.out.println("\nIngrese un numero del array: ");
+        int valor1 = Integer.parseInt(System.console().readLine());
+        System.out.println("Ingrese un numero para reemplazarlo: ");
+        int valor2 = Integer.parseInt(System.console().readLine());
+        System.out.println("\n");
+
+        for ( i = 0; i < numeros.length; i++) {
+            
+            if (numeros[i] == valor1) {
+                numeros[i] = valor2;
+                System.out.print("*"+numeros[i]+"*\t ");
+            }else{
+                System.out.print(numeros[i]+"\t ");
+            }
+        }
+
 
     }
 }
