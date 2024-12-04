423482f0c9ca4d5855282ea518b0c92d4fbbd539
Eduardo <echavez@utea.edu.pe>
2024-12-04 10:07:27 -0500

    Ejercicio 2 suma filas

--- Arreglos/EjerArrayBi02.java
+++ Arreglos/EjerArrayBi02.java
@@ -8,9 +8,20 @@ public class EjerArrayBi02 {
         System.out.println("Ingrese los numeros (enteros) en el array");
         for (f = 0; f < 4; f++) {
             for (c = 0; c < 5; c++) {
-                System.out.print("Fila " + f + ", Columna " + c + ": ");
-                num[f][c] = Integer.parseInt(System.console().readLine());
+                //System.out.print("Fila " + f + ", Columna " + c + ": ");
+                num[f][c] = (int) ((Math.random()*50)+1);//Integer.parseInt(System.console().readLine());
             }
+        }
+
+        // Suma de las filas
+        int sumaFila;
+        for (f = 0; f < 4; f++) {
+            sumaFila = 0;
+            for (c = 0; c < 5; c++) {
+                System.out.printf("%7d  ", num[f][c]);
+                sumaFila = sumaFila + num[f][c];
+            }
+            System.out.printf("|%7d\n", sumaFila);
         }
     }
 }
