59afcb7eca59ea45f1ca4545c364c722682192f0
Eduardo <echavez@utea.edu.pe>
2024-12-06 20:37:18 -0500

    Ejercicio 03 ArrayBi completo

--- Arreglos/EjerArrayBi03.java
+++ Arreglos/EjerArrayBi03.java
@@ -6,22 +6,22 @@ public class EjerArrayBi03 {
         int f, c;
         int minimo = Integer.MAX_VALUE;
         int maximo = Integer.MIN_VALUE;
-        int fMin =0;
-        int cMin=0;
-        int fMax=0;
-        int cMax=0;
+        int fMin = 0;
+        int cMin = 0;
+        int fMax = 0;
+        int cMax = 0;
 
-        for ( f = 0; f < 6; f++) {
+        for (f = 0; f < 6; f++) {
             for (c = 0; c < 10; c++) {
                 num[f][c] = (int) (Math.random() * 1001);
                 System.out.printf("%5d", num[f][c]);
-                //Calcular el minimo
+                // Calcular el minimo
                 if (num[f][c] < minimo) {
                     minimo = num[f][c];
                     fMin = f;
                     cMin = c;
                 }
-                //Calcular el maximo
+                // Calcular el maximo
                 if (num[f][c] > maximo) {
                     maximo = num[f][c];
                     fMax = f;
@@ -30,8 +30,8 @@ public class EjerArrayBi03 {
             }
             System.out.println();
         }
-        System.out.println("\n el mínimo valor es: " + minimo + " y esta en la posición " + fMin + ", "+cMin);
-        System.out.println("\n el maximo valor es: " + maximo + " y esta en la posición " + fMax + ", "+cMax);
+        System.out.println("\n el mínimo valor es: " + minimo + " y esta en la posición " + fMin + ", " + cMin);
+        System.out.println("\n el maximo valor es: " + maximo + " y esta en la posición " + fMax + ", " + cMax);
     }
 
 }
