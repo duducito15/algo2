47409e0a9fb479390f6bd190fb00a4d927a5748c
Eduardo <echavez@utea.edu.pe>
2024-12-23 10:59:33 -0500

    Poo

---
+++ Poo/Libro.java
@@ -0,0 +1,22 @@
+package Poo;
+/*
+POO (Programacion Orientada a Objetos), es un paradigma de programacion, la clave es que todo
+es un objeto que toman el nombre de instancia, un objeto tiene caracteristicas se conocen
+como atributos o variables de la instancia.
+
+Clase: es una abstracion que denota cierta cualidades
+Instancia: objeto palpable que deriva de una clase
+Atributos: conjunto de caracteristicas que comparten los objetos de la clase
+
+*/
+
+public class Libro {
+    //Atributos de la clase
+    String isbn;
+    String titulo;
+    String autor;
+    String portada;
+    int numeroPaginas;
+}
+
+
---
+++ Poo/PruebaLibro.java
@@ -0,0 +1,11 @@
+package Poo;
+
+public class PruebaLibro {
+
+    public static void main(String[] args) {
+        Libro Coquito = new Libro();
+        Libro Movil = new Libro();
+
+    }
+
+}
