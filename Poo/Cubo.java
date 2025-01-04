4eabf7d81fa377746d1ac61f1903af8bcb780da6
Eduardo <echavez@utea.edu.pe>
2025-01-03 19:17:02 -0500

    Cubo Pinta

--- Poo/Cubo.java
+++ Poo/Cubo.java
@@ -27,4 +27,25 @@ public class Cubo {
         void setContenido(int litros){
             this.contenido = litros;
         }
+
+        //otros metodos
+        void vacia(){
+            this.contenido = 0;
+        }
+
+        void llena(){
+            this.contenido = this.capacidad;
+        }
+
+        //Pinta cubo en la pantalla
+        void pintaCubo(){
+            for (int nivel = this.capacidad; nivel > 0; nivel--) {
+                if (this.contenido >= nivel) {
+                    System.out.println("#~~~~#");
+                } else {
+                    System.out.println("#    #");
+                }
+                System.out.println("######");
+            }
+        }
 }
---
+++ Poo/PruebaCubo.java
@@ -0,0 +1,10 @@
+package Poo;
+
+public class PruebaCubo {
+    public static void main(String[] args) {
+        Cubo miCubito = new Cubo(2);
+        Cubo miCubote = new Cubo(7);
+
+        
+    }
+}
