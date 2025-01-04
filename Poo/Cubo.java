50c98da1edcab41eea106b7d417f4c26ebad1130
Eduardo <echavez@utea.edu.pe>
2025-01-03 19:36:29 -0500

    Volcar contenido de un cubo

--- Poo/Cubo.java
+++ Poo/Cubo.java
@@ -1,51 +1,67 @@
 package Poo;
 
 public class Cubo {
-    //Atributos
+    // Atributos
     int capacidad; // litros maximo
     int contenido; // litros actuales
 
-    //Metodos
-        //Construtor
-        Cubo(int c){
-            this.capacidad = c;
-        }
+    // Metodos
+    // Construtor
+    Cubo(int c) {
+        this.capacidad = c;
+    }
 
-        //getter
-        int getCapacidad(){
-            return this.capacidad;
-        }
-        int getContenido(){
-            return this.contenido;
-        }
+    // getter
+    int getCapacidad() {
+        return this.capacidad;
+    }
 
-        //Setter
-        void setCapacidad(int litros){
-            this.capacidad = litros;
-        }
+    int getContenido() {
+        return this.contenido;
+    }
 
-        void setContenido(int litros){
-            this.contenido = litros;
-        }
+    // Setter
+    void setCapacidad(int litros) {
+        this.capacidad = litros;
+    }
 
-        //otros metodos
-        void vacia(){
-            this.contenido = 0;
-        }
+    void setContenido(int litros) {
+        this.contenido = litros;
+    }
 
-        void llena(){
-            this.contenido = this.capacidad;
-        }
+    // otros metodos
+    void vacia() {
+        this.contenido = 0;
+    }
 
-        //Pinta cubo en la pantalla
-        void pintaCubo(){
-            for (int nivel = this.capacidad; nivel > 0; nivel--) {
-                if (this.contenido >= nivel) {
-                    System.out.println("#~~~~#");
-                } else {
-                    System.out.println("#    #");
-                }
-                System.out.println("######");
+    void llena() {
+        this.contenido = this.capacidad;
+    }
+
+    // Pinta cubo en la pantalla
+    void pintaCubo() {
+        for (int nivel = this.capacidad; nivel > 0; nivel--) {
+            if (this.contenido >= nivel) {
+                System.out.println("#~~~~#");
+            } else {
+                System.out.println("#    #");
             }
         }
+        System.out.println("######");
+    }
+
+    // Volcar el contenido de un cubo sobre otro
+    void vuelvaEn(Cubo destino) {
+        int libres = destino.getCapacidad() - destino.getContenido();
+
+        if (libres > 0) {
+            if (this.contenido <= libres) {
+                destino.setContenido(destino.getContenido() + this.contenido);
+                this.vacia();
+            } else {
+                this.contenido -= libres; // this.contenido = this.contenido - libres;
+                destino.llena();
+            }
+        }
+    }
 }
--- Poo/PruebaCubo.java
+++ Poo/PruebaCubo.java
@@ -5,6 +5,18 @@ public class PruebaCubo {
         Cubo miCubito = new Cubo(2);
         Cubo miCubote = new Cubo(7);
 
-        
+        System.out.println("Cubito \n");
+        miCubito.pintaCubo();
+
+        System.out.println("Cubote \n");
+        miCubote.pintaCubo();
+
+        System.out.println("Llena Cubito \n");
+        miCubito.llena();
+        miCubito.pintaCubo();
+
+        System.out.println("Llena Cubote \n");
+        miCubote.llena();
+        miCubote.pintaCubo();
     }
 }
