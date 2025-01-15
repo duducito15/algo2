724dad25340a44e6802f64336c333671058531de
Eduardo <echavez@utea.edu.pe>
2025-01-15 11:50:05 -0500

    ArrayList color

--- Listas/ArrayList1.java
+++ Listas/ArrayList1.java
@@ -15,14 +15,31 @@ public class ArrayList1 {
         myList.add("verde");
         myList.add("morado");
         myList.add("azul");
+        myList.add("rosado");
+        myList.add("negro");
+        myList.add("marron");
 
         System.out.println("Nro elementos: "+myList.size());
         System.out.println("el elemento que hay en la posicion 1 es: "+myList.get(1));
 
-        System.out.println("Contenido de la lista");
+        //System.out.println("Contenido de la lista");
         //for (int i = 0; i < myList.size(); i++) {
         //    System.out.println(myList.get(i));
         //}
+        //for(String color: myList ){
+        //    System.out.println(color);
+        //}
+        String colorete;
+        System.out.println("Ingrese un color para verificar en el listado: ");
+        colorete = System.console().readLine();
+
+        if (myList.contains(colorete)) {
+            System.out.println("El "+colorete+" existe en la lista de colores");
+        } else{
+            System.out.println("El "+colorete+", No existe en la lista de colores, lo agregamos");
+            myList.add(colorete);
+        }
+        System.out.println("Nuevo contenido de la lista");
         for(String color: myList ){
             System.out.println(color);
         }
