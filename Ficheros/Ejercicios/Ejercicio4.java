827f653e4440290f8dba743270f0309c5b964212
Duducito15 <echavezv@utea.edu.pe>
2025-02-03 12:58:21 -0500

    Ejercicio 4

---
+++ Ficheros/Ejercicios/Ejercicio4.java
@@ -0,0 +1,39 @@
+package Ficheros.Ejercicios;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Vector;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(".//Ficheros//Ejercicios//palabras.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(".//Ficheros//Ejercicios//palabras_sort.txt"));
+
+            ArrayList<String> a = new ArrayList<String>();
+
+            String linea = "";
+            while (linea != null) {
+                a.add(linea);
+                linea = br.readLine();
+            }
+            br.close();
+
+            a.remove(0);
+            Collections.sort(a);
+
+            for (String s : a) {
+                bw.write(s + "\n");
+            }
+            bw.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+    }
+}
